6ea5283f48d9d42c98b4a62c695f22f59bf07d3b
dianerdiana <dianerdiana.de@gmail.com>
2023-03-23 13:45:34 +0700

    Implement promise

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -3,40 +3,38 @@ const main = function () {
   const buttonSearchElement = document.querySelector('#searchButtonElement')
   const clubListElement = document.querySelector('#clubList')
 
-  const onButtonSearchClicked = function () {
-    const dataSource = new DataSource(renderResult, fallbackResult)
-    dataSource.searchClub(searchElement.value)
+  const onButtonSearchClicked = async () => {
+    try {
+      const results = await DataSource.searchClub(searchElement.value)
+      renderResult(results)
+    } catch (error) {
+      fallbackResult(error)
+    }
   }
 
-  const renderResult = function (results) {
+  const renderResult = (results) => {
     clubListElement.innerHTML = ''
-    results.forEach(function (club) {
-      const name = club.name
-      const fanArt = club.fanArt
-      const description = club.description
+    results.forEach((club) => {
+      const { name, fanArt, description } = club
 
       const clubElement = document.createElement('div')
       clubElement.setAttribute('class', 'club')
 
-      clubElement.innerHTML =
-        '<img class="fan-art-club" src="' +
-        fanArt +
-        '" alt="Fan Art">\n' +
-        '<div class="club-info">\n' +
-        '<h2>' +
-        name +
-        '</h2>\n' +
-        '<p>' +
-        description +
-        '</p>' +
-        '</div>'
+      clubElement.innerHTML = `
+        <img class="fan-art-club" src=${fanArt} alt="Fan Art">
+        <div class="club-info">
+          <h2>${name}</h2>
+          <p>${description}</p>
+        </div>
+      `
+
       clubListElement.appendChild(clubElement)
     })
   }
 
-  const fallbackResult = function (message) {
+  const fallbackResult = (message) => {
     clubListElement.innerHTML = ''
-    clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
   }
 
   buttonSearchElement.addEventListener('click', onButtonSearchClicked)
