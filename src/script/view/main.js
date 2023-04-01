e9d6e79ec4e71d6eea7771be54ccb8dc71f757d3
dianerdiana <dianerdiana.de@gmail.com>
2023-04-01 21:45:29 +0700

    Fetch club use API

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,29 +1,43 @@
-import '../../components/ClubList.js'
-import '../../components/SearchBar.js'
-import DataSource from '../data/data-source.js'
+import '../../components/ClubList.js';
+import '../../components/SearchBar.js';
+
+const BASE_URL = 'https://sports-api.dicoding.dev';
 
 const main = () => {
-  const searchElement = document.querySelector('search-bar')
-  const clubListElement = document.querySelector('club-list')
+  const searchElement = document.querySelector('search-bar');
+  const clubListElement = document.querySelector('club-list');
 
   const onButtonSearchClicked = async () => {
     try {
-      const results = await DataSource.searchClub(searchElement.value)
-      renderResult(results)
+      const responseRaw = await fetch(
+        `${BASE_URL}/teams/search?t=${searchElement.value}`
+      );
+      const results = await responseRaw.json();
+      const teams = results.teams.map((team) => {
+        const t = {
+          id: team.idTeam,
+          name: team.strTeam,
+          fanArt: team.strTeamBadge,
+          description: team.strDescriptionEN
+        };
+
+        return t;
+      });
+      renderResult(teams);
     } catch (error) {
-      fallbackResult(error)
+      fallbackResult(error);
     }
-  }
+  };
 
   const renderResult = (results) => {
-    clubListElement.clubs = results
-  }
+    clubListElement.clubs = results;
+  };
 
   const fallbackResult = (message) => {
-    clubListElement.renderError(message)
-  }
+    clubListElement.renderError(message);
+  };
 
-  searchElement.clickEvent = onButtonSearchClicked
-}
+  searchElement.clickEvent = onButtonSearchClicked;
+};
 
-export default main
+export default main;
